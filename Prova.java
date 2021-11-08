ce4f0985148f3b18e399182bebcc10f0c8b907f1
sarahdari <93925459+sarahdari@users.noreply.github.com>
2021-11-08 15:17:45 +0100

    Update Prova.java

--- Prova.java
+++ Prova.java
@@ -2,6 +2,6 @@ public class Prova{
 
 	public static void main(String...args){
 	
-		System.out.println("Come funziona sta roba?");
+		System.out.println("Come funziona sta roba????????????");
 	}
 }
